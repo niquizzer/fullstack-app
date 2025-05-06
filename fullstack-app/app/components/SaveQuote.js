3438376ab63c4441f94b2f0e02509a9a725f4164
Nequea <nequeacoleman@gmail.com>
2025-05-06 08:51:07 -0600

    designed how the app would work a bit better

--- fullstack-app/app/components/SaveQuote.js
+++ fullstack-app/app/components/SaveQuote.js
@@ -1,6 +1,10 @@
-
+import useSelector from 'react-redux';
 
 const SaveQuote = () => {
+    const currentQuote = useSelector((state) => state.currentQuote);
+    const currentAuthor = useSelector((state) => state.currentAuthor);
+    const savedQuotes = useSelector((state) => state.savedQuotes);
+
    const saveCurrentQuote = () => {
     
    } 
